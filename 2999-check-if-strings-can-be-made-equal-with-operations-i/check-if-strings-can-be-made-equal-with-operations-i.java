3fea1da2685a0fe4183654edfaef4024f57c5a8a
vigneshmuthukumar2910 <vickylanzer2910@gmail.com>
2026-03-31 11:37:27 +0530

    Time: 2 ms (26.75%) | Memory: 43.9 MB (98.36%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        char[] even1=new char[]{s1.charAt(0),s1.charAt(2)};
+        char[] even2=new char[]{s2.charAt(0),s2.charAt(2)};
+        char[] odd1=new char[]{s1.charAt(1),s1.charAt(3)};
+        char[] odd2=new char[]{s2.charAt(1),s2.charAt(3)};
+        Arrays.sort(even1);
+        Arrays.sort(even2);
+        Arrays.sort(odd1);
+        Arrays.sort(odd2);
+        return Arrays.equals(even1,even2) && Arrays.equals(odd1,odd2);
+    }
+}
